ad3836c038baae76ce4e49717dc5ac7579005b5c
riley <rebeccakbartels@gmail.com>
2020-01-30 17:52:14 -0500

    fixed client side error

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Marco Santonastaso - Personal Website</title>
+	<title>rebeccabartels - Personal Website</title>
 	<!-- meta -->
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
